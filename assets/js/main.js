9bfd417a4f6382c6611abbbf0d942b5fedd8f012
Fabian Gutierrez <fabiangtml@gmail.com>
2021-11-15 20:26:50 -0600

    Adding 'Scroll to' Functionality and nav close

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,36 @@
 /*============SHOW MENU==================*/
 
 const navToggle = document.querySelector('.nav_toggle'),
-nav = document.querySelector('.nav_list')
+navMenu = document.querySelector('.nav_list'),
+navClose = document.querySelector('.show-menu'),
+main = document.getElementById('main')
+
 
 /* Validate if constant exists */
-
 if(navToggle){
     navToggle.addEventListener('click', () =>{
-        nav.classList.add('show-menu')
+        navMenu.classList.add('show-menu')
     })
 }
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(main){
+    main.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.querySelector('.nav_list')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*============GSAP Hero section==================*/
@@ -253,3 +274,23 @@ gsap.from(".about_info" , {
     opacity: 0,
     duration: 1.5,
 })
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_cont a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_cont a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
--- email.php
+++ email.php
@@ -1,7 +1,6 @@
 <?php
-if(isset($_POST['submit'])){
     session_start();
-    
+if(isset($_POST['submit'])){
     //Get all the values from post 
     $target = "fabiangtml@gmail.com";
     $name = $_POST['name'];
@@ -12,8 +11,9 @@ if(isset($_POST['submit'])){
     setcookie("name", $name, time() + 60*60*24*30);
     setcookie("email", $email, time() + 60*60*24*30);
     //Send Message 
+    $headers = "From: ".$email;
     $msg = "Name: ".$name."\n"."Email: ".$email."\n"."Message: ".$message;
-    mail($target, $subject, $msg);
+    mail($target, $subject, $msg, $headers);
     mail($email, $subject, "Thank you for your message, please wait until i see your email");
     $_SESSION['email_send'] = '<p>Email sended Successfully!</p>';
     header("location:index.php");
